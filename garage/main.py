dc523767c7877ac0a342a31f9d2db71ee3ba6a05
Stephen Harding <harding@spawar.navy.mil>
2019-04-09 20:04:08 -0700

    added some commentary to remind me how to set this up in the future

--- garage/main.py
+++ garage/main.py
@@ -1,3 +1,11 @@
 # main.py
 
+# This is the main file for the garage door opener.
+# put onto esp8266 by running:
+# ampy -p /dev/ttyUSB0 put main.py
+#
+# This project also needs garage_opener.py and common.py
+# added to the board in the same way.
+
+
 import garage_opener
