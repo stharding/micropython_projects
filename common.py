47e522c6eee93f59083cb954b7b655f33082c012
Stephen Harding <harding@spawar.navy.mil>
2019-04-09 20:00:04 -0700

    reorganized a little

---
+++ common.py
@@ -0,0 +1,11 @@
+# common.py
+
+def connect_to_wifi(ssid, password):
+    wlan = WLAN(STA_IF)
+    wlan.active(True)
+    if not wlan.isconnected():
+        print('connecting to network...')
+        wlan.connect(ssid, password)
+        while not wlan.isconnected():
+            pass
+    print('network config:', wlan.ifconfig())
---
+++ garage/garage_opener.py
@@ -0,0 +1,42 @@
+# garage_opener.py
+
+from machine import Pin
+from network import WLAN, STA_IF
+from time import sleep
+
+# from library installed by: upip.install('micropython-umqtt.simple')
+from umqtt import simple
+
+# from my common module
+from common import connect_to_wifi
+
+MQTT_BROKER = '192.168.0.1'
+
+
+def main():
+    # pin labeled '01' on my esp8266
+    pin = Pin(5, Pin.OUT)
+
+    def handle_garage_button(topic, msg):
+        print(topic, msg)
+        pin.on()
+        mqtt_client.publish(b'garage_btn/sts', b'ON')
+        sleep(1)
+        pin.off()
+        mqtt_client.publish(b'garage_btn/sts', b'OFF')
+
+
+    connect_to_wifi('wifi-ssid', 'wifi-password')
+
+    mqtt_client = simple.MQTTClient('garage_opener', MQTT_BROKER)
+    mqtt_client.set_callback(handle_garage_button)
+    mqtt_client.connect()
+    print('connected to mqtt broker!')
+    mqtt_client.subscribe('garage_btn/cmd')
+
+
+    while True:
+        mqtt_client.wait_msg()
+
+
+main()
